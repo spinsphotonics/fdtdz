fixed multiple outputs to use the more python (start, stop, step) formula

src/fdtdz_jax/fdtdz_jax.py
@@ -191,7 +191,7 @@ def fdtdz(
       layers used in the simulation (`bot + top`) is required to be a multiple
       of `4`.
 
-    output_steps: `(start, interval, stop)` tuple of integers denoting the
+    output_steps: `(start, stop, interval)` tuple of integers denoting the
       update step at which to start recording output fields, the number of
       update steps separating successive output fields, and the step at which
       to stop recording (not included).
@@ -261,8 +261,8 @@ def fdtdz(
         f"[0, {yy if source_field.ndim == 3 else zz}) but got {source_position}."
     )
 
-  out_start, out_interval, out_stop = output_steps
-  out_num = (out_stop - out_start) // out_interval
+  out_start, out_stop, out_interval = output_steps
+  out_num = len(range(out_start, out_stop, out_interval))
   tt = out_start + out_interval * (out_num - 1) + 1
 
   if not (source_waveform.ndim == 2 and source_waveform.shape == (tt, 2)):
@@ -349,7 +349,7 @@ def fdtdz(
   abslayer = jnp.pad(abslayer, ((0, 0), (4, pxx - xx - 4), (4, pyy - yy - 4)))
   srclayer = (
       jnp.pad(source_field,
-              ((0, 0), (0, 0), (4, pxx - xx - 4), (4, pyy - yy - 4)))  # 
+              ((0, 0), (0, 0), (4, pxx - xx - 4), (4, pyy - yy - 4)))  #
       if source_field.ndim == 4  #
       else jnp.pad(source_field, ((0, 0), (4, pxx - xx - 4), (0, 0))))
   zcoeff = jnp.stack(

tests/test_fdtdz_jax.py
@@ -1,19 +1,17 @@
+import fdtdz_jax
+from jax.test_util import check_grads
+from jax.config import config
+import jax.numpy as jnp
+import jax
+import pytest
 import numpy as np
 
 np.set_printoptions(threshold=np.inf)
 
-import pytest
-import jax
-import jax.numpy as jnp
-from jax.config import config
-from jax.test_util import check_grads
-
-import fdtdz_jax
-
 
 # TODO: Tests are missing!!!
 def test_fdtdz():
-  xx, yy, zz, tt = 16, 16, 44, 1
+  xx, yy, zz, tt = 16, 16, 44, 100
   epsilon = np.ones((3, xx, yy, zz), np.float32)
   dt = float(0.5)
   source_field = np.zeros((2, 2, xx, yy), np.float32)
@@ -25,7 +23,7 @@ def test_fdtdz():
   pml_kappa = np.ones((zz, 2), np.float32)
   pml_sigma = np.zeros((zz, 2), np.float32)
   pml_widths = (10, 10)
-  output_steps = (tt - 1, 1, tt)
+  output_steps = (tt - 10 - 1, tt, 10)
   use_reduced_precision = False
   launch_params = "Quadro RTX 4000"
 
@@ -46,3 +44,4 @@ def test_fdtdz():
 
   print(out[0, 0, :, :, zz // 2])
   print(f"Does output contain NaN values? {np.any(np.isnan(out))}")
+  print(out.shape)