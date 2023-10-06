81f20c6b6cf9e00ebf07d5b5c4393b60257d0b30
Jesse Lu <jesselu@hollisterphotonics.com>
2023-10-06 17:47:57 +0000

    make compatible with jax 0.14.16

--- src/fdtdz_jax/fdtdz_jax.py
+++ src/fdtdz_jax/fdtdz_jax.py
@@ -593,7 +593,7 @@ def _fdtdz_lowering(ctx, cbuffer, abslayer, srclayer, waveform, zcoeff,
 
   out = custom_call(
       "kernel_f16" if kwargs["use_reduced_precision"] else "kernel_f32",
-      out_types=[
+      result_types=[
           mlir.ir.RankedTensorType.get(shapes["buffer"],
                                        mlir.ir.F32Type.get()),
           mlir.ir.RankedTensorType.get(shapes["cbuffer"],
